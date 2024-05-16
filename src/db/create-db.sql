9a636a82062819f7992268bc09172539baccdfbf
Natalia Kędziora <n.m.kedziora@gmail.com>
2024-05-16 16:57:12 +0200

    Add mysql integration (dirty)

---
+++ src/db/create-db.sql
@@ -0,0 +1,17 @@
+/*
+Creates the 'wit' database and 3 users:
+  * witadmin with full permissions to the database
+  * witapp to be used by the web application
+  * witreader with read-only access
+*/
+
+CREATE DATABASE wit;
+
+CREATE USER 'witadmin'@'%' IDENTIFIED BY 'witadmin';
+GRANT ALL ON wit.* TO 'witadmin'@'%';
+
+CREATE USER 'witapp'@'%' IDENTIFIED WITH mysql_native_password BY 'witapp';
+GRANT INSERT, UPDATE, DELETE, SELECT ON wit.* TO 'witapp'@'%';
+
+CREATE USER 'witreader'@'%' IDENTIFIED BY 'witreader';
+GRANT SELECT ON wit.* TO 'witreader'@'%';
